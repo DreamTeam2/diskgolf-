027796a5e13936170ec6b203c79974e7d4f3ff92
Dusan Matejka <dusky@dusky.local>
2016-01-27 01:13:35 +0100

    Adding defaults to day picker of turnaj Fixes #7

--- Frisbee/turnaj/models.py
+++ Frisbee/turnaj/models.py
@@ -1,12 +1,20 @@
 from django.db import models
-from datetime import date
+from datetime import date, timedelta
 # Create your models here.
 from django.utils.encoding import smart_unicode
 
 class Turnaj(models.Model):
     nazov = models.CharField(max_length = 50)
-    datum_od = models.DateField(default=date.today)
-    datum_do = models.DateField(default=date.today)
+    
+    day = date.today().weekday()
+    datum_od = None
+    datum_do = None
+    if day < 6: 
+        datum_od = models.DateField(default=(date.today() - timedelta(days=day+2)))
+        datum_do = models.DateField(default=(date.today() - timedelta(days=day+1)))
+    else:
+        datum_od = models.DateField(default=(date.today() - timedelta(days=1)))
+        datum_do = models.DateField(default=(date.today()))
     mesto = models.CharField(max_length = 50, default = '')
     stat = models.CharField(max_length = 50, default = '')
     datum_zapisu = models.DateField(default=date.today)
