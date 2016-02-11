cc0a41b052eace80030fe2acbf4dd3cf88872808
mirelon <miso@github.ksp.sk>
2016-02-11 23:30:09 +0100

    Separating custom settings fixed #1

--- Frisbee/Frisbee/settings.py
+++ Frisbee/Frisbee/settings.py
@@ -85,19 +85,7 @@ WSGI_APPLICATION = 'Frisbee.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default':{
-        'ENGINE':'django.db.backends.mysql',
-        'NAME': 'frisbee',
-        'USER': 'root',
-        'PASSWORD': '0000',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    'OPTIONS': {
-         "init_command": "SET foreign_key_checks = 0;",
-    },
-    }
-}
+from custom_settings import *
 
 
 TEMPLATE_CONTEXT_PROCESSORS = TCP + [
